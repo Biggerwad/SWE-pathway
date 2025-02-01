f309a9b435c761f56b41cce08ebefafa40fa5704
Daniel Wesey <weseydaniel74@gmail.com>
2025-02-01 10:20:31 -0600

    week1 commit and warm-ups

---
+++ completedProjects/wk1proj/task_manager.py
@@ -0,0 +1,16 @@
+# 
+""""
+project: Task manager
+Strength:
+
+About:
+- use list of dicts to store tasks
+
+functions
+   - Adding tasks  
+   - Viewing tasks  
+   - Marking tasks as done  
+   - Deleting tasks  
+   - Saving/loading tasks from a file  
+
+"""
---
+++ make.py
@@ -0,0 +1,7 @@
+x = "Hello Sir"
+price = 23
+amount = input("How many items: ")
+
+result = f"The cost of food is: {price:.2f} dollars"
+
+print(result)
